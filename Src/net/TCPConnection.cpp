d1ecbf59ede4126c106a179ed9e2e13fe6032cf6
Think5UP <85314390@qq.com>
2023-03-12 20:01:11 +0800

    update

--- Src/net/TCPConnection.cpp
+++ Src/net/TCPConnection.cpp
@@ -16,7 +16,7 @@ static net_lib::EventLoop* checkNotNull(net_lib::EventLoop* loop){
     return loop;
 }
 
-namespace net_lib{
+namespace net_lib {
     TCPConnection::TCPConnection(EventLoop *loop, const std::string &name,
                                  int sockfd, const InetaAddress &localAddr,
                                  const InetaAddress &peerAddr)
@@ -27,8 +27,8 @@ namespace net_lib{
               localAddr_(localAddr),
               peerAddr_(peerAddr),
               socket_(new Socket(sockfd)),
-              channel_(new Channel(loop_,sockfd)),
-              highWaterMark_(64*1024*1024){
+              channel_(new Channel(loop_, sockfd)),
+              highWaterMark_(64 * 1024 * 1024) {
         channel_->setReadEventCallBack(
                 std::bind(&TCPConnection::handleRead, this, std::placeholders::_1)
         );
@@ -41,12 +41,12 @@ namespace net_lib{
         channel_->setErrorEventCallBack(
                 std::bind(&TCPConnection::handleError, this)
         );
-        LOG_INFO("TCPConnection ctor[%s] at fd=%d\n",name_.c_str(),sockfd);
+        LOG_INFO("TCPConnection ctor[%s] at fd=%d\n", name_.c_str(), sockfd);
         socket_->setKeepAlive(true);
     }
 
     TCPConnection::~TCPConnection() {
-        LOG_INFO("TCPConnection dtor[%s] at fd=%d state=%s\n",name_.c_str(),channel_->fd(),StateToString().c_str());
+        LOG_INFO("TCPConnection dtor[%s] at fd=%d state=%s\n", name_.c_str(), channel_->fd(), StateToString().c_str());
     }
 
     const std::string TCPConnection::StateToString() {
@@ -67,25 +67,36 @@ namespace net_lib{
     }
 
     void TCPConnection::send(Buffer *buf) {
-
+        if(state_ == kConnected){
+            if(loop_->isInLoopThread()){
+                sendInLoop(buf->peek(),buf->readableBytes());
+                buf->retrieveAll();
+            } else {
+                void(TCPConnection::*p)(const std::string &message) = &TCPConnection::sendInLoop;
+                loop_->runInLoop(std::bind(p,this,buf->retrieveAllAsString()));
+            }
+        }
     }
 
     void TCPConnection::send(const std::string &buf) {
-        if(state_ == kConnected){
-            if(loop_->isInLoopThread()){
+        if (state_ == kConnected) {
+            if (loop_->isInLoopThread()) {
                 sendInLoop(buf.c_str(), buf.size());
             } else {
-                void(TCPConnection::*p)(const void* data,size_t len) = &TCPConnection::sendInLoop;
+                void (TCPConnection::*p)(const void *data, size_t len) = &TCPConnection::sendInLoop;
                 loop_->runInLoop(std::bind(p,
-                                 this,
-                                 buf.c_str(),
-                                 buf.size()));
+                                           this,
+                                           buf.c_str(),
+                                           buf.size()));
             }
         }
     }
 
     void TCPConnection::shutdown() {
-
+        if(state_ == kConnected){
+            setState(kDisConnecting);
+            loop_->queueInLoop(std::bind(&TCPConnection::shutdownInLoop, this));
+        }
     }
 
     void TCPConnection::ConnectEstablish() {
@@ -103,8 +114,9 @@ namespace net_lib{
         connectionCallBack_(shared_from_this());
 
     }
+
     void TCPConnection::ConnectDestroy() {
-        if(state_ == kConnected){
+        if (state_ == kConnected) {
             setState(kDisConnected);
             channel_->disAllEvent();
             connectionCallBack_(shared_from_this());
@@ -116,52 +128,49 @@ namespace net_lib{
         int savedErrno = 0;
         // TcpConnection会从socket读取数据，然后写入inpuBuffer
         ssize_t n = inputBuffer_.readfd(channel_->fd(), &savedErrno);
-        if (n > 0)
-        {
+        if (n > 0) {
             // 已建立连接的用户，有可读事件发生，调用用户传入的回调操作
             // TODO:shared_from_this
-            messageCallBack_(shared_from_this(),&inputBuffer_,receiveTime);
-        }
-        else if (n == 0)
-        {
+            messageCallBack_(shared_from_this(), &inputBuffer_, receiveTime);
+        } else if (n == 0) {
             // 没有数据，说明客户端关闭连接
             handleClose();
-        }
-        else
-        {
+        } else {
             // 出错情况
             errno = savedErrno;
             LOG_ERROR("TcpConnection::handleRead() failed\n");
             handleError();
         }
     }
+
     void TCPConnection::handelWrite() {
-        if (channel_->isWriting()){
+        if (channel_->isWriting()) {
             ssize_t n = ::write(channel_->fd(),
                                 outputBuffer_.peek(),
                                 outputBuffer_.readableBytes());
-            if(n > 0){
+            if (n > 0) {
                 //重置readerIndex_的位置，后移n个字节，如果写完就重置位
                 outputBuffer_.retrieve(n);
-                if(outputBuffer_.readableBytes() == 0){
+                if (outputBuffer_.readableBytes() == 0) {
                     //说明已经将数据全部写入channel了,关闭channel可写
                     channel_->disableWriteEvent();
-                    if(writeCompleteCallBack_){
+                    if (writeCompleteCallBack_) {
                         //函数结束后调用回调
                         loop_->queueInLoop(std::bind(&TCPConnection::writeCompleteCallBack_,
                                                      shared_from_this()));
                     }
-                    if(state_ == kDisConnecting){
+                    if (state_ == kDisConnecting) {
                         shutdownInLoop();
                     }
                 } else {
                     LOG_ERROR("TCPConnection::handelWrite failed\n");
                 }
             } else {
-                LOG_ERROR("TCPConnection::handelWrite fd=[%d]\n",channel_->fd());
+                LOG_ERROR("TCPConnection::handelWrite fd=[%d]\n", channel_->fd());
             }
         }
     }
+
     void TCPConnection::handleClose() {
         setState(kDisConnected);
         channel_->disAllEvent();
@@ -169,53 +178,70 @@ namespace net_lib{
         connectionCallBack_(connPtr);
         closeCallBack_(connPtr);
     }
+
     void TCPConnection::handleError() {
         int err = 0;
         int optval = 0;
         socklen_t optlen = sizeof(optval);
-        if(::getsockopt(channel_->fd(),SOL_SOCKET,SO_ERROR,&optval,&optlen)){
+        if (::getsockopt(channel_->fd(), SOL_SOCKET, SO_ERROR, &optval, &optlen)) {
             err = errno;
         } else {
             err = optval;
         }
-        LOG_ERROR("TCPConnection::handleError name[%s],SO_ERROR=%d\n",name_.c_str(),err);
+        LOG_ERROR("TCPConnection::handleError name[%s],SO_ERROR=%d\n", name_.c_str(), err);
 
     }
 
     void TCPConnection::sendInLoop(const std::string &message) {
-
+        sendInLoop(message.data(),message.size());
     }
-    void TCPConnection::sendInLoop(const void* message, size_t len) {
+
+    void TCPConnection::sendInLoop(const void *message, size_t len) {
         int nwrote = 0;
         int remainning = len;
         bool defaultError = false;
-        if(state_ == kDisConnected){
+        if (state_ == kDisConnected) {
             LOG_ERROR("TCPConnection is close give up writing");
         }
         //如果这是第一次发送数据,并且缓冲区中没有数据
-        if(!channel_->isWriting() && outputBuffer_.readableBytes() == 0){
-            nwrote = ::write(channel_->fd(),message,len);
-            if(nwrote >= 0){
+        if (!channel_->isWriting() && outputBuffer_.readableBytes() == 0) {
+            nwrote = ::write(channel_->fd(), message, len);
+            if (nwrote >= 0) {
                 remainning = len - nwrote;
                 //一次直接将数据全部写完了,那就直接执行回调就好了
-                if(remainning == 0 && writeCompleteCallBack_){
-                    loop_->queueInLoop(std::bind(writeCompleteCallBack_,shared_from_this()));
+                if (remainning == 0 && writeCompleteCallBack_) {
+                    loop_->queueInLoop(std::bind(writeCompleteCallBack_, shared_from_this()));
                 }
             } else {
                 //写入出错
                 nwrote = 0;
-                if(errno != EWOULDBLOCK)//排除因为非阻塞导致的错误
+                if (errno != EWOULDBLOCK)//排除因为非阻塞导致的错误
                 {
-                    if(errno == EPIPE || errno == ECONNRESET){
+                    if (errno == EPIPE || errno == ECONNRESET) {
                         defaultError = true;
                     }
                 }
             }
         }
-
+        if (!defaultError && remainning > 0) {
+            size_t oldLen = outputBuffer_.readableBytes();
+            if (oldLen + remainning > highWaterMark_ &&
+                oldLen < highWaterMark_ &&
+                highWaterMarkCallBack_) {
+                loop_->queueInLoop(std::bind(TCPConnection::highWaterMarkCallBack_,
+                                             shared_from_this(),
+                                             oldLen + remainning));
+            }
+            outputBuffer_.append((char *) message + nwrote, remainning);
+            if (!channel_->isWriting()) {
+                channel_->enableWriteEvent();
+            }
+        }
     }
-
-    void TCPConnection::shutdownInLoop(){
-
+    void TCPConnection::shutdownInLoop() {
+        //outputBuffer中的数据全部写完。
+        if(!channel_->isWriting()){
+            socket_->shutDownWrite();
+        }
     }
 }
